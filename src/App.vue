b63bd0c6c4a7b55c2856254d7ef348bed5766422
ilborg228 <asd120200@gmail.com>
2021-10-25 12:44:27 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<p>a</p>
+  <button @click="fetchEndpoints" >AAAAAAA</button>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  methods:{
+    async fetchEndpoints(){
+      try {
+        const response = await axios.get('http://localhost:8080/endpoints')
+        console.log(response)
+      }
+      catch (e) {
+        alert(e)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from "@/App";
+//npm run serve -- --port 3000
+createApp(App).mount('#app')
